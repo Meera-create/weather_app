3ea6e02730052528c93f48f41972671e6dcf6a92
Meera A <mirunaalini.alagarajah@gmail.com>
2023-10-10 11:31:36 +0100

    working

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css'
 import ForecastDetails from './ForecastDetails';
 import axios from 'axios';
-import { response } from 'express';
 import getForecast from './getForecast';
 
 const App = () =>{
